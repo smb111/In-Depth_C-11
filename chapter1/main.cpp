10f74b7e6604d96f1f7ab00c6b45d5b07cc64973
smb111 <skymyweakness@gmail.com>
2018-11-19 20:48:13 +0800

    decltype规则3

--- chapter1/main.cpp
+++ chapter1/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
-
 using namespace std;
+
+class A {
+public:
+	int i = 0;
+};
+
+
+class Foo {
+public:
+	int x;
+};
 
 int main()
 {
 	auto i = 5;
-
+	
 	cout << i << endl;
+
+	decltype(i) j = 1;
+	
+
+	const Foo foo = Foo();
+
+	decltype(foo.x) a = 3;
+	decltype((foo.x)) b = a;
+	a = 3;
+	// b = 4; b : const int &
+
 
 	return 0;
 }
