343613de231985e539686adf93f2410b29d0afb0
smb111 <skymyweakness@gmail.com>
2018-11-20 10:55:25 +0800

    使用 using 关键词代替 typedef 重定义模板类型

--- chapter1/main.cpp
+++ chapter1/main.cpp
@@ -17,6 +17,32 @@ auto func1(T t, R r) -> decltype(t + r)
 	return t + r;
 }
 
+template <typename T>
+class B{
+public:
+	T t;
+};
+
+template <typename T>
+class C{
+public:
+	T t;
+};
+
+#include <map>
+#include <string>
+typedef unsigned int uint;
+typedef std::map<std::string, int> map_str_t;
+/* C++ 无法使用 typedef 直接重定义模板类型，必须使用关键字 using */
+// template <typename T>
+// typedef std::map<T, std::string> map_T_str;
+
+/* 使用 using 关键字代替 typedef 定义模板类型 */
+template <typename Val>
+using str_map_T = std::map<std::string, Val>;
+str_map_T<int> map1;
+
+
 int main()
 {
 	auto i = 5;
@@ -32,6 +58,13 @@ int main()
 	// b = 4; b : const int &
 
 	cout << func1<int , int>(3,5) << endl;
+	
+	B<C<int>> bc;
+
+	map_str_t mymap;
+	mymap["string"] = 1;
+	cout << mymap["string"] << endl;
 
 	return 0;
 }
+
